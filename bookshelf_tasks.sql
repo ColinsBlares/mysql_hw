6492c256f9650dbba7c3bd69db17099f85d8894d
ColinsBlares <118037471+ColinsBlares@users.noreply.github.com>
2023-06-09 16:08:30 +0300

    Update bookshelf_tasks.sql

--- bookshelf_tasks.sql
+++ bookshelf_tasks.sql
@@ -38,8 +38,12 @@ JOIN shelves ON books.shelves_id = shelves.id
 WHERE (shelves.title LIKE 'верхняя%') OR (shelves.title LIKE 'нижняя%');
 -- 8. 
 UPDATE books
-SET books.friends_id = 1
-WHERE id = 9;
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+SET books.friends_id = (SELECT friends.id FROM friends WHERE friends.name = 'Иванов Иван')
+WHERE authors.name = 'Данте Алигьери' AND books.title = 'Божественная комедия'
+AND books.id > 0;
+
 -- 9. 
 INSERT INTO books (books.title, books.year, books.shelves_id)
 VALUES ('Краткие ответы на большие вопросы', '2020', 1);
