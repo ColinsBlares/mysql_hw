d3ff58731b999c53814179a04e7b539759520796
ColinsBlares <118037471+ColinsBlares@users.noreply.github.com>
2023-06-14 12:50:01 +0300

    Update movies_tasks.sql

--- movies_tasks.sql
+++ movies_tasks.sql
@@ -1,38 +1,38 @@
---1.
+-- 1.
 SELECT count(*) FROM movies
 JOIN companies ON movies.companies_id = companies.id
 WHERE companies.title = 'Universal Pictures'
 ;
---2.
+-- 2.
 SELECT count(*) FROM movies
 JOIN directors ON movies.directors_id = directors.id
 WHERE directors.full_name = 'Фрэнсис Форд Коппола'
 ;
---3.
+-- 3.
 SELECT count(*) FROM movies
 WHERE year > year(curdate()) - 20
 ;
---4.
+-- 4.
 SELECT DISTINCT genres.title FROM movies
 JOIN genres ON movies.id_genres = genres.id
 JOIN directors ON movies.id_directors = directors.id
 WHERE directors.full_name = 'Стивен Спилберг'
 ;
---5.
+-- 5.
 SELECT movies.title, genres.title, directors.full_name FROM movies
 JOIN genres ON movies.genres_id = genres.id
 JOIN directors ON movies.id_directors = directors.id
 ORDER BY budget DESC
 LIMIT 5
 ;
---6.
+-- 6.
 SELECT directors.full_name FROM movies
 JOIN directors ON movies.directors_id = directors.id
 GROUP BY directors.full_name
 ORDER BY COUNT(directors.full_name) DESC
 LIMIT 1
 ;
---7.
+-- 7.
 SELECT movies.title, genres.title FROM movies
 JOIN genres ON movies.genres_id = genres.id
 JOIN companies ON movies.companies_id = companies.id
@@ -43,18 +43,18 @@ GROUP BY companies.title
 ORDER BY SUM(movies.budget) DESC
 LIMIT 1)
 ;
---8.
+-- 8.
 SELECT avg(movies.budget) FROM movies
 JOIN companies ON movies.companies_id = companies.id
 WHERE companies.title = 'Warner Bros.'
 GROUP BY companies.titl
 ;
---9.
+-- 9.
 SELECT genres.title, COUNT(movies.id), AVG(movies.budget) FROM movies
 JOIN genres ON movies.genres_id = genres.id
 GROUP BY genres.title
 ;
---10.
+-- 10.
 DELETE FROM movies
 WHERE id = (SELECT id FROM (SELECT movies.id FROM movies
 JOIN genres ON movies.genres_id
